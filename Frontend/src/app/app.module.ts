749abb9d113d188687ce45f883e5be0086e8faa5
Oleh Maksymiv <olegmaksumiv@gmail.com>
2021-07-09 13:45:03 +0300

    user-register forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 
 const appRoutes: Routes = [
@@ -18,6 +20,8 @@ const appRoutes: Routes = [
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: PropertyListComponent}
 ]
 
@@ -30,12 +34,15 @@ const appRoutes: Routes = [
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
---
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,54 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  registerationForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.registerationForm = new FormGroup({
+      userName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.minLength(10)])
+    });
+  }
+
+ //passwordMatchingValidator(fg: FormGroup): Validators {
+ // return fg.get('password').value === fg.get('confirmPassword').value ? null :
+ // {notmatched: true };
+//}
+
+ get userName(){
+   return this.registerationForm.get('userName') as FormControl;
+ }
+
+ get email(){
+  return this.registerationForm.get('email') as FormControl;
+}
+
+get password(){
+  return this.registerationForm.get('password') as FormControl;
+}
+
+get confirmPassword(){
+  return this.registerationForm.get('confirmPassword') as FormControl;
+}
+
+get mobile(){
+  return this.registerationForm.get('mobile') as FormControl;
+}
+
+  onSubmit(){
+    console.log(this.registerationForm);
+  }
+}
