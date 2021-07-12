952d74c2e5cce4e605726345f0c78a080c5c3c1c
Oleh Maksymiv <olegmaksumiv@gmail.com>
2021-07-12 19:58:47 +0300

    added image gallery

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { UserRegisterComponent } from './user/user-register/user-register.compon
 import { AlertifyService } from './services/alertify.service';
 import { AuthService } from './services/auth.service';
 import { UserServiceService } from './services/user-service.service';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 
@@ -57,7 +58,8 @@ const appRoutes: Routes = [
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     ButtonsModule.forRoot(),
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    NgxGalleryModule
   ],
   providers: [
     HousingService,
--- Frontend/src/app/property/property-detail/property-detail.component.ts
+++ Frontend/src/app/property/property-detail/property-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Property } from 'src/app/model/property';
 import { HousingService } from 'src/app/services/housing.service';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-property-detail',
@@ -12,6 +15,8 @@ export class PropertyDetailComponent implements OnInit {
 
   public propertyId: any;
   property = new Property();
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
 
   constructor(private route: ActivatedRoute,
@@ -47,12 +52,42 @@ export class PropertyDetailComponent implements OnInit {
             this.property.Description = data.Description;
             this.property.Address = data.Address;
             this.property.Address2 = data.Address2;
-
-
           }
         )
       }
     );
+
+
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '465px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide
+
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/internal-1.jpg',
+        medium: 'assets/images/internal-1.jpg',
+        big: 'assets/images/internal-1.jpg'
+      },
+      {
+        small: 'assets/images/internal-2.jpg',
+        medium: 'assets/images/internal-2.jpg',
+        big: 'assets/images/internal-2.jpg'
+      },
+      {
+        small: 'assets/images/internal-3.jpg',
+        medium: 'assets/images/internal-3.jpg',
+        big: 'assets/images/internal-3.jpg'
+      }
+    ];
+
+
+
   }
 
 }
